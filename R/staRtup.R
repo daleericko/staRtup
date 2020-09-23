b3d3bef6f45ba2e9661d32ce54a8617e41071b0d
Erick Cohen <erickcohen@ericks-air.lan>
2020-09-23 13:12:18 -0400

    made staRtup()

---
+++ R/staRtup.R
@@ -0,0 +1,25 @@
+#' StaRtup - create the base folders you need for your R project quickly
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' staRtup()
+staRtup <- function() {
+
+  data_raw <- here::here("data-raw")
+  data_output <- here::here("data-output")
+  scripts <- here::here("R-scripts")
+  resources <- here::here("resources")
+
+  dirs <- list(data_raw, data_output, scripts, resources)
+
+  for (dir in dirs) {
+    if (!dir.exists(dir)) {
+      dir.create(dir)
+    } else {
+      message(paste("This directory already exists:", dir))
+    }
+  }
+
+}
